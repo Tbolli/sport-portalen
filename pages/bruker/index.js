e9626d2bcd36639ec0ff25d24150a24ff46dfadb
Tbolli <thomas.bolli@outlook.com>
2022-05-12 13:03:32 +0200

    time conver

--- pages/bruker/index.js
+++ pages/bruker/index.js
@@ -1,21 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link' 
-import styles from '../../styles/Login.module.css'
+import styles from '../../styles/Feed.module.css'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-import { sha256, sha224 } from 'js-sha256';
 import firebase from 'firebase/app'
 import 'firebase/firestore'
-import { collectionGroup, getFirestore,collection, addDoc, getDocs, query, where   } from "firebase/firestore";
-
-import app from '../../firebase/initFirebase'
 
 export default function index() {
-  const db = getFirestore(app)
   const router = useRouter();
   const usrNameQuery = router.query.username
+  
   const [qdata, setQData] = useState([])
 
   useEffect(()=>{
@@ -28,17 +24,40 @@ export default function index() {
     const jsonData = await responseObj.json()
     setQData(jsonData)
   }
+const card =({type,title,info,location,time,when,hosts,coming})=>{
+  return(
+  <div className={styles.item}>
+    <h1>{type?"Trening":"Kamp"} - {title}</h1>
+    <p>{info}</p>
+    <div>
+      <span>{location}</span>
+      <span>{time}</span>
+    </div>
 
-
+  </div>
   
-
-    console.log(qdata)
+  )
+}
+  console.log(qdata)
   return (
       <>
-      <h1>{usrNameQuery}</h1>
-      
-      <button onClick={fetchQueryResult}>asdd</button>
+      {qdata.map(doc=>(
+        card(doc)
+
+
+
+      ))}
       </>
 
     )
 }
+
+
+function timeConvert(n) {
+  var num = n;
+  var hours = (num / 60);
+  var rhours = Math.floor(hours);
+  var minutes = (hours - rhours) * 60;
+  var rminutes = Math.round(minutes);
+  return rhours + " t, " + rminutes + " min";
+  }
